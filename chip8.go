6432f4520efd3e7aa7fe14416b74106b026d48bf
braheezy <michael.c.braha@gmail.com>
2024-02-06 19:53:09 -0800

    Implement math opcodes

--- chip8.go
+++ chip8.go
@@ -1,5 +1,7 @@
 package main
 
+import "math/rand"
+
 const (
 	// In CHIP-8, the program starts at address 0x200.
 	programStartAddress = 0x200
@@ -144,15 +146,93 @@ func (ch8 *CHIP8) stepInterpreter() {
 		// 6XNN: Store number NN in register VX.
 		register := instruction.nibbles(1, 1)
 		value := instruction.nibbles(2, 3)
-		debug("[%04X] Loading %X into register %d\n", instruction, value, register)
+		debug("[%04X] Loading %X into V%d\n", instruction, value, register)
 		ch8.V[register] = byte(value)
 
 	case 0x7:
 		// 7XNN: Add the value NN to register VX
 		register := instruction.nibbles(1, 1)
 		value := instruction.nibbles(2, 3)
-		debug("[%04X] Adding %X to contents of register %d\n", instruction, value, register)
+		debug("[%04X] Adding %X to contents of V%d\n", instruction, value, register)
 		ch8.V[register] += byte(value)
+
+	case 0x8:
+		// Arithmetic and logical operators
+		registerX := instruction.nibbles(1, 1)
+		registerY := instruction.nibbles(2, 2)
+		lastNibble := instruction.nibbles(3, 3)
+		switch lastNibble {
+		case 0x0:
+			// 8XY0: Store the value of register VY in register VX.
+			debug("[%04X] Loading contents of V%d into V%d\n", instruction, registerY, registerX)
+			ch8.V[registerX] = ch8.V[registerY]
+
+		case 0x1:
+			// 8XY1: Set VX to VX OR VY.
+			debug("[%04X] Loading (V%d | V%d) into V%d\n", instruction, registerY, registerX, registerX)
+			ch8.V[registerX] |= ch8.V[registerY]
+
+		case 0x2:
+			// 8XY2: Set VX to VX AND VY.
+			debug("[%04X] Loading (V%d & V%d) into V%d\n", instruction, registerY, registerX, registerX)
+			ch8.V[registerX] &= ch8.V[registerY]
+
+		case 0x3:
+			// 8XY3: Set VX to VX XOR VY.
+			debug("[%04X] Loading (V%d XOR V%d) into V%d\n", instruction, registerY, registerX, registerX)
+			ch8.V[registerX] ^= ch8.V[registerY]
+
+		case 0x4:
+			// 8XY4: Add the value of register VY to register VX
+			// Set VF to 01 if a carry occurs
+			// Set VF to 00 if a carry does not occur
+			debug("[%04X] Adding V%d to V%d and storing into V%d\n", instruction, registerY, registerX, registerX)
+			ch8.V[0xF] = 0
+			sum := uint16(ch8.V[registerX]) + uint16(ch8.V[registerY])
+			if sum > 0xFF {
+				ch8.V[0xF] = 1
+			}
+			ch8.V[registerX] += ch8.V[registerY]
+
+		case 0x5:
+			// 8XY5: Subtract the value of register VY from register VX
+			// Set VF to 00 if a borrow occurs
+			// Set VF to 01 if a borrow does not occur
+			debug("[%04X] Subtracting V%d from V%d and storing into V%d\n", instruction, registerY, registerX, registerX)
+			ch8.V[0xF] = 1
+			if ch8.V[registerY] > ch8.V[registerX] {
+				ch8.V[0xF] = 0
+			}
+			ch8.V[registerX] -= ch8.V[registerY]
+
+		case 0x6:
+			// 8XY6: Store the value of register VY shifted right one bit in register VX
+			// Set VF to the least significant bit prior to the shift.
+			// TODO: COSMAC VIP: Set VX to value in VY first
+			debug("[%04X] Shifting V%d right and storing into V%d\n", instruction, registerY, registerX)
+			ch8.V[0xF] = ch8.V[registerY] & 0x1
+			ch8.V[registerX] = ch8.V[registerY] >> 1
+
+		case 0x7:
+			// 8XY7: Subtract the value of register VX from register VY
+			// Set VF to 00 if a borrow occurs
+			// Set VF to 01 if a borrow does not occur
+			debug("[%04X] Subtracting V%d from V%d and storing into V%d\n", instruction, registerX, registerY, registerX)
+			ch8.V[0xF] = 1
+			if ch8.V[registerX] > ch8.V[registerY] {
+				ch8.V[0xF] = 0
+			}
+			ch8.V[registerX] = ch8.V[registerY] - ch8.V[registerX]
+
+		case 0xE:
+			// 8XYE: Store the value of register VY shifted left one bit in register VX
+			// Set VF to the least significant bit prior to the shift.
+			// TODO: COSMAC VIP: Set VX to value in VY first
+			debug("[%04X] Shifting V%d right and storing into V%d\n", instruction, registerY, registerX)
+			ch8.V[0xF] = ch8.V[registerY] >> 7
+			ch8.V[registerX] = ch8.V[registerY] << 1
+
+		}
 
 	case 0x9:
 		// 9XY0: Skip the next instruction if VX does not equal VY.
@@ -168,6 +248,21 @@ func (ch8 *CHIP8) stepInterpreter() {
 		value := instruction.nibbles(1, 3)
 		debug("[%04X] Loading %03X into I\n", instruction, value)
 		ch8.I = value
+
+	case 0xB:
+		// BNNN: Jump to the address NNN plus V0.
+		// TODO: CHIP-48 and SUPER-CHIP: Jump to BXNN
+		value := instruction.nibbles(1, 3)
+		debug("[%04X] Setting pc to %03X + V0\n", instruction, value)
+		ch8.pc = value + uint16(ch8.V[0])
+
+	case 0xC:
+		// CXNN: Set VX to a random number AND NN.
+		value := instruction.nibbles(2, 3)
+		registerX := instruction.nibbles(1, 1)
+		randomNumber := rand.Intn(256)
+		debug("[%04X] Setting V%X to (%d AND %X)\n", instruction, registerX, randomNumber, value)
+		ch8.V[registerX] = byte(randomNumber & int(value))
 
 	case 0xD:
 		// DXYN" Draw a sprite at position VX, VY with N bytes of sprite data starting at the address stored in I
@@ -198,7 +293,7 @@ func (ch8 *CHIP8) stepInterpreter() {
 				} else {
 					// Reset this pixel
 					if ch8.display.content[drawX+byte(x)][drawY+uint8(y)] != 0 {
-						// This pixel was set, was turn on VF flag.
+						// This pixel was set, so turn on VF flag.
 						ch8.V[0xF] = 1
 					}
 				}
